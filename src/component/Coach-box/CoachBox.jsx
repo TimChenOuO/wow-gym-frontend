91a4d87963f7ebc79d73374bc216db98dce1200f
Yu-Linchien <yu@Yu-LinchiendeMacBook-Pro.local>
2020-07-02 07:01:05 +0800

    rwd課程表375px待修改，人數無法即時更新

--- src/component/Coach-box/CoachBox.jsx
+++ src/component/Coach-box/CoachBox.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './CoachBox.scss'
 import Swal from "sweetalert2";
 
--- src/component/Course-box/CourseBox.jsx
+++ src/component/Course-box/CourseBox.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./CourseBox.scss";
 
 import CourseBookingButton from "../course-booking-button/CourseBookingButton";
